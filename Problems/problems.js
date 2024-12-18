6a8a42d11f2d17b38252ec0fc6bd90d5a9bc02de
Pawan Kumar <cnct.pawan@gmail.com>
2024-12-18 19:33:55 +0530

    Added some problems

---
+++ Problems/problems.js
@@ -0,0 +1,49 @@
+// Write a loop that makes seven calls to console.log to output the following triangle:
+
+let triangle = "";
+for (let index = 1; index < 7; index++) {
+    triangle += "*";
+
+    console.log(triangle);
+}
+
+
+/* Write a program that uses console.log to print all the numbers from 1 to 100,
+with two exceptions. For numbers divisible by 3, print "Fizz" instead of the
+number, and for numbers divisible by 5 (and not 3), print "Buzz" instead.
+When you have that working, modify your program to print "FizzBuzz" for
+numbers that are divisible by both 3 and 5 (and still print "Fizz" or "Buzz"
+for numbers divisible by only one of those) */
+
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+}
+
+/* Write a program that creates a string that represents an 8×8 grid, using newline
+characters to separate lines. At each position of the grid there is either a space
+or a "#" character. The characters should form a chessboard */
+
+let size = 8;
+let board = "";
+
+for (let y = 0; y < size; y++) {
+    for (let x = 0; x < size; x++) {
+        if ((x + y) % 2 === 0) {
+            board += " ";
+        } else {
+            board += "*"
+        }
+    }
+board += "\n";
+}
+console.log(board);
+
